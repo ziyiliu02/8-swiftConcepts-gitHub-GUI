d6a8ace191d8eab47d2fde5eb60a4f8ccea01154
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-09 15:58:44 +0800

    Add text

--- 8-swiftConcepts-gitHub-GUI/ContentView.swift
+++ 8-swiftConcepts-gitHub-GUI/ContentView.swift
@@ -11,10 +11,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("GitHub Desktop App (GUI)")
+                .font(.title)
+            
+            Text("Branching")
         }
         .padding()
     }
