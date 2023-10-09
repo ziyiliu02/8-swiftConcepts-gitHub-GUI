b83fdcff706bf833817478a8c1d1c3f96061a72c
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-09 15:13:35 +0800

    Initial Commit

---
+++ 8-swiftConcepts-gitHub-GUI/__swiftConcepts_gitHub_GUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftConcepts_gitHub_GUIApp.swift
+//  8-swiftConcepts-gitHub-GUI
+//
+//  Created by Liu Ziyi on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct __swiftConcepts_gitHub_GUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
